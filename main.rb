01589946b4cc41a1a654aeb307de1b3425cf0d32
rubycrafter <anton_freeman@inbox.ru>
2015-10-16 16:59:57 +0500

    Работает обход дерева сайта

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ include Scanner
 CATALOG = 'http://www.a-yabloko.ru/catalog/'
 HEADERS = %w(type group pic name)
 GROUP_TAG = '#catalog-content .children a'
-SUBGROUP_TAG = '#content.bar h1 .children a'
+SUBGROUP_TAG = '#content.bar .children a'
 PRODICT_TAG = '#content.bar h1 div.goods .img'
 STATISTICS = Statistics.new
 
@@ -18,10 +18,9 @@ page = Mechanize.new.get(CATALOG)
 group_links = scan_main(page)
 
 group_links.each do |link|
-#  scan_group(link)
+  scan_page(link)
 end
 
-puts group_links.inspect
 #group_links = page.search(GROUP_TAG).map { |link| link['href'] }
 
 #groups = group_links.map { |link| page.link_with(href: link).click }
--- scanner.rb
+++ scanner.rb
@@ -1,13 +1,44 @@
 module Scanner
-  def scan_main(page)
-    group_row = page.search(GROUP_TAG).map do |row| 
+  class << self
+    attr_accessor :depth
+  end
+
+  def parse(page, tag, type, group)
+    row = page.search(tag).map do |row| 
       name = row.text.delete('0-9')
       pic = File.basename(row['style'].scan(/url\((.*)\)/).join) || '-'
-      new_record('group', '-', name, pic)
+      new_record(type, group, name, pic)
+      print 'DEBUG: Scanning ', group, '; subgroups: ', name, "\n"
       row['href']
     end
-    group_links = group_row.map { |link| page.link_with(href: link) }
-    group_links 
+    links = row.map { |link| page.link_with(href: link) }
+    links
+  end
+
+  def scan_main(page)
+    Scanner.depth = 0
+    parse(page, GROUP_TAG, 'group', '-')    
+  end
+
+  def scan_page(link)
+    Scanner.depth
+    page = link.click
+    group = link.text.delete('0-9')
+    subgroups = find_subgroups(page, group)
+    if subgroups.empty?
+      scan_goods(link, group)
+    else
+      subgroups.each { |subgroup| scan_page(subgroup) }
+    end
+  end
+
+  def find_subgroups(link, group)
+    type = 'sub-' * Scanner.depth + 'group'
+    subgroups = parse(link, SUBGROUP_TAG, type, group)
+  end
+
+  def scan_goods(link, group)
+    
   end
 
   def new_record(type, group, name, pic)
