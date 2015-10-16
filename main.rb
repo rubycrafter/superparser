e16462b0e5d09ac23f2f5304e00f8acfea4da7b9
rubycrafter <anton_freeman@inbox.ru>
2015-10-16 18:15:48 +0500

    Работает обход дерева сайта, теперь корректно

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
-require 'csv'
 require 'mechanize'
 load 'statistics.rb'
-load 'page.rb'
 load 'scanner.rb'
 
 include Scanner
@@ -10,7 +8,7 @@ CATALOG = 'http://www.a-yabloko.ru/catalog/'
 HEADERS = %w(type group pic name)
 GROUP_TAG = '#catalog-content .children a'
 SUBGROUP_TAG = '#content.bar .children a'
-PRODICT_TAG = '#content.bar h1 div.goods .img'
+PRODUCT_TAG = '#content.bar .goods .img'
 STATISTICS = Statistics.new
 
 page = Mechanize.new.get(CATALOG)
@@ -21,6 +19,3 @@ group_links.each do |link|
   scan_page(link)
 end
 
-#group_links = page.search(GROUP_TAG).map { |link| link['href'] }
-
-#groups = group_links.map { |link| page.link_with(href: link).click }
